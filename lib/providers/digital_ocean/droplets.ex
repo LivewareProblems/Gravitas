8b10e55a7e296a2aca685923a877f619d0052139
Thomas Depierre <depierre.thomas@gmail.com>
2019-04-18 18:10:43 +0200

    adds pagination

--- lib/providers/digital_ocean/droplets.ex
+++ lib/providers/digital_ocean/droplets.ex
@@ -8,8 +8,10 @@ defmodule Gravitas.Providers.DigitalOcean.Droplets do
       |> Map.put(:path, "/v2/droplets")
       |> Map.put(:http_method, :get)
 
-    case DigitalOcean.perform(operation, DigitalOcean.Config.new(override_opts)) do
-      {:ok, resp} -> {:ok, Jason.decode!(resp[:body]) |> Map.get("region_info", [])}
+    do_config = DigitalOcean.Config.new(override_opts)
+
+    case get_paginated_data(operation, "region_info", do_config, []) do
+      {:ok, list_resp} -> {:ok, list_resp}
       {:error, reason} -> {:error, reason}
     end
   end
@@ -23,6 +25,32 @@ defmodule Gravitas.Providers.DigitalOcean.Droplets do
 
     case DigitalOcean.perform(operation, DigitalOcean.Config.new(override_opts)) do
       {:ok, resp} -> {:ok, Jason.decode!(resp[:body]) |> Map.get("droplet", %{})}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def get_paginated_data(operation, key, config, results) do
+    with {:ok, resp} <- DigitalOcean.perform(operation, config),
+         body <- Jason.decode!(resp[:body]) do
+      body_key_results = Map.get(body, key, [])
+
+      case body do
+        %{"links" => %{"next" => next_link}} ->
+          with %URI{} = uri <- URI.parse(next_link),
+               query_params <- URI.decode_query(uri.query),
+               do_config <- Map.merge(config, uri) |> Map.put(:params, query_params) do
+            get_paginated_data(operation, key, do_config, [results, body_key_results])
+          else
+            error -> error
+          end
+
+        %{"links" => _links} ->
+          {:ok, List.flatten(results, body_key_results)}
+
+        _ ->
+          {:ok, body_key_results}
+      end
+    else
       {:error, reason} -> {:error, reason}
     end
   end
