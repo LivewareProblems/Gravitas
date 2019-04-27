552cf023e4fd0e375f7ff1ac0d2f0372095cd326
Thomas Depierre <depierre.thomas@gmail.com>
2019-04-27 20:20:34 +0200

    adds create_droplets to digital ocean provider

--- lib/gravitas/providers/digital_ocean/droplets.ex
+++ lib/gravitas/providers/digital_ocean/droplets.ex
@@ -16,12 +16,26 @@ defmodule Gravitas.Providers.DigitalOcean.Droplets do
     end
   end
 
-  @spec get_droplets_by_id(integer() | String.t(), list()) :: {:error, any()} | {:ok, any()}
+  @spec get_droplets_by_id(integer() | String.t(), keyword()) :: {:error, any()} | {:ok, any()}
   def get_droplets_by_id(id, override_opts \\ []) do
     operation =
       DigitalOcean.Operation.new()
       |> Map.put(:path, "/v2/droplets/" <> to_string(id))
       |> Map.put(:http_method, :get)
+
+    case DigitalOcean.perform(operation, DigitalOcean.Config.new(override_opts)) do
+      {:ok, resp} -> {:ok, Jason.decode!(resp[:body]) |> Map.get("droplet", %{})}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  @spec create_droplet(map(), keyword) :: {:error, any()} | {:ok, map()}
+  def create_droplet(droplet_data, override_opts \\ []) do
+    operation =
+      DigitalOcean.Operation.new()
+      |> Map.put(:path, "/v2/droplets/")
+      |> Map.put(:http_method, :post)
+      |> Map.put(:data, droplet_data)
 
     case DigitalOcean.perform(operation, DigitalOcean.Config.new(override_opts)) do
       {:ok, resp} -> {:ok, Jason.decode!(resp[:body]) |> Map.get("droplet", %{})}
