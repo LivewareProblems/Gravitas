546b43ccf905e711a602acf2df5370d3f478a96f
Thomas Depierre <depierre.thomas@gmail.com>
2019-07-10 19:21:07 +0200

    adds a shell.nix

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+let
+  inherit (lib) optional optionals;
+
+  elixir = beam.packages.erlangR21.elixir_1_8;
+in
+
+mkShell {
+  buildInputs = [ elixir dhall ]
+    ++ optional stdenv.isLinux inotify-tools # For file_system on Linux.
+    ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+      # For file_system on macOS.
+      CoreFoundation
+      CoreServices
+    ]);
+  # Fix GLIBC Locale
+  LOCALE_ARCHIVE = stdenv.lib.optionalString stdenv.isLinux
+    "${pkgs.glibcLocales}/lib/locale/locale-archive";
+}
